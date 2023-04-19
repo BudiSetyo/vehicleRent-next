3d2a725ca911621f71afd5968a4d56bf3057fbc7
BudiSetyo <budisetyo176@gmail.com>
2023-04-19 22:34:42 +0700

    fix: pagination on vehicle type

--- pages/vehicleType/category/[type].js
+++ pages/vehicleType/category/[type].js
@@ -4,40 +4,64 @@ import {
   Cards,
   AutoComplete,
   Pagination,
+  Loading,
 } from "@/components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { useSelector } from "react-redux";
-import { axios } from "axios";
+// import { useSelector } from "react-redux";
+import axios from "axios";
 const api = process.env.API_URL;
 
 const VehicleType = () => {
   const router = useRouter();
   const { type } = router.query;
 
-  const vehicleData = useSelector((state) => state.vehicles);
+  //   const vehicleData = useSelector((state) => state.vehicles);
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
+  const [data, setData] = useState({
+    results: [],
+    total: 0,
+  });
   const [currentPage, setCurrentPage] = useState(1);
 
   const handleNavigate = (href) => {
     return router.push(href);
   };
 
-  const fetchData = () => {
+  const fetchData = (page) => {
     setLoading(!loading);
 
     axios({
       method: "get",
-      url: ``,
+      url:
+        type === "popular"
+          ? `${api}/vehicles/?popular=true&page=${page}&row=4`
+          : type === "cars"
+          ? `${api}/vehicles/?type=a5d6059c-5899-4d81-8e01-72cd58515044&page=${page}&row=4`
+          : type === "moto-bike"
+          ? `${api}/vehicles/?type=2a2823f0-ddc6-4c25-8f8c-791f7bdeea76&page=${page}&row=4`
+          : `${api}/vehicles/?type=a0acce4f-6149-4e21-83cf-fff17fafae03&page=${page}&row=4`,
     })
-      .then()
-      .catch();
+      .then((result) => {
+        setLoading(false);
+        setData(result.data.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
   };
+
+  const handlePage = (page) => fetchData(page);
+
+  useEffect(() => {
+    handlePage(currentPage);
+  }, [currentPage]);
 
   return (
     <MainLayout>
+      {loading ? <Loading /> : <></>}
+
       <section className="md:py-8 md:px-20 py-4 px-10">
         <AutoComplete />
 
@@ -56,31 +80,19 @@ const VehicleType = () => {
             }
             hidden={true}
           >
-            {vehicleData
-              .filter((vehicles) =>
-                type === "popular"
-                  ? vehicles.isPopular
-                  : type === "cars"
-                  ? vehicles.type === "car"
-                  : type === "moto-bike"
-                  ? vehicles.type === "moto bike"
-                  : type === "bike"
-                  ? vehicles.type === "bike"
-                  : vehicles
-              )
-              .map((vehicle, index) => {
-                return (
-                  <Cards
-                    key={index}
-                    name={vehicle.name}
-                    location={vehicle.location}
-                    image={vehicle.picture}
-                    onClick={() => {
-                      handleNavigate(`/vehicleType/detail/${vehicle.id}`);
-                    }}
-                  />
-                );
-              })}
+            {data.results.map((vehicle, index) => {
+              return (
+                <Cards
+                  key={index}
+                  name={vehicle.name}
+                  location={vehicle.location}
+                  image={vehicle.picture}
+                  onClick={() => {
+                    handleNavigate(`/vehicleType/detail/${vehicle.id}`);
+                  }}
+                />
+              );
+            })}
           </VehicleList>
         </section>
 
@@ -88,8 +100,8 @@ const VehicleType = () => {
           <Pagination
             className="pagination-bar"
             currentPage={currentPage}
-            totalCount={100}
-            pageSize={10}
+            totalCount={data.total}
+            pageSize={4}
             onPageChange={(page) => setCurrentPage(page)}
           />
         </div>
